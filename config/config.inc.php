52bd580e237285e8a4dc9777db0a6551d17ddb01
Fitria Damayanti <fitria.damayanti.996@gmail.com>
2025-04-11 18:01:19 +0700

    Update

--- config/config.inc.php
+++ config/config.inc.php
@@ -15,7 +15,7 @@ $DBMS = getenv('DBMS') ?: 'MySQL';
 # If you are using MariaDB then you cannot use root, you must use create a dedicated DVWA user.
 #   See README.md for more information on this.
 $_DVWA = array();
-$_DVWA[ 'db_server' ]   = getenv('DB_SERVER') ?: '127.0.0.1';
+$_DVWA[ 'db_server' ]   = getenv('DB_SERVER') ?: 'localhost';
 $_DVWA[ 'db_database' ] = getenv('DB_DATABASE') ?: 'dvwa';
 $_DVWA[ 'db_user' ]     = getenv('DB_USER') ?: 'dvwa';
 $_DVWA[ 'db_password' ] = getenv('DB_PASSWORD') ?: 'p@ssw0rd';
@@ -30,7 +30,7 @@ $_DVWA[ 'recaptcha_private_key' ] = getenv('RECAPTCHA_PRIVATE_KEY') ?: '';
 # Default security level
 #   Default value for the security level with each session.
 #   The default is 'impossible'. You may wish to set this to either 'low', 'medium', 'high' or impossible'.
-$_DVWA[ 'default_security_level' ] = getenv('DEFAULT_SECURITY_LEVEL') ?: 'impossible';
+$_DVWA[ 'default_security_level' ] = getenv('DEFAULT_SECURITY_LEVEL') ?: 'low';
 
 # Default locale
 #   Default locale for the help page shown with each session.
@@ -40,7 +40,7 @@ $_DVWA[ 'default_locale' ] = getenv('DEFAULT_LOCALE') ?: 'en';
 # Disable authentication
 #   Some tools don't like working with authentication and passing cookies around
 #   so this setting lets you turn off authentication.
-$_DVWA[ 'disable_authentication' ] = getenv('DISABLE_AUTHENTICATION') ?: false;
+$_DVWA[ 'disable_authentication' ] = getenv('DISABLE_AUTHENTICATION') ?: true;
 
 define ('MYSQL', 'mysql');
 define ('SQLITE', 'sqlite');
